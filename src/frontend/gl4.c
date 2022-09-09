d67ef045b6d0af7e6b147596af422b9cab4407b8
Aritz Erkiaga <aerkiaga3@gmail.com>
2022-09-09 23:18:52 +0200

    A few changes to the OpenGL code

--- src/frontend/gl4.c
+++ src/frontend/gl4.c
@@ -77,8 +77,8 @@ const char* fragment_shader =
 
 const char* effects_vertex_shader =
 "#version 400\n"
-"in vec4 vp;"
-"out vec2 UV;"
+"attribute vec4 vp;"
+"varying vec2 UV;"
 "void main() {"
 "  gl_Position = vp;"
 "  UV = vp.xy * vec2(0.5, 0.5) + vec2(0.5, 0.5);"
@@ -86,12 +86,12 @@ const char* effects_vertex_shader =
 
 const char* effects_fragment_shader =
 "#version 400\n"
-"in vec2 UV;"
-"uniform sampler2D tex;"
-"uniform sampler2D depth;"
+"varying vec2 UV;"
+"uniform sampler2D texRGB;"
+"uniform sampler2D texDepth;"
 "void main() {"
-"  gl_FragColor = texture2D(tex, UV);"
-"  gl_FragDepth = texture2D(depth, UV).x;"
+"  gl_FragColor = texture2D(texRGB, UV);"
+"  gl_FragDepth = texture2D(texDepth, UV).x;"
 "}";
 
 GLuint shader_program = 0;
@@ -108,6 +108,9 @@ GLuint depth_tex = 0;
 GLuint effects_program = 0;
 GLuint quad_VBO = 0;
 GLuint quad_VAO = 0;
+
+GLuint texRGB = 0;
+GLuint texDepth = 0;
 
 const GLfloat quad_vertices[] = {
     -1.0f, -1.0f, 0.0f,
@@ -135,22 +138,6 @@ void frontend_realize_graphics(int scale_factor) {
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
     
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    if(glGetError()) printf("error: could not create and bind VAO.\n");
-    
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_DYNAMIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    if(glGetError()) printf("error: could not create and bind VBO.\n");
-    
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 0, NULL, GL_DYNAMIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    if(glGetError()) printf("error: could not create and bind EBO.\n");
-    
     int status;
     glGenFramebuffers(1, &tmp_FBO);
     glBindFramebuffer(GL_FRAMEBUFFER, tmp_FBO);
@@ -168,6 +155,22 @@ void frontend_realize_graphics(int scale_factor) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_tex, 0);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    if(glGetError()) printf("error: could not create and bind VAO.\n");
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    if(glGetError()) printf("error: could not create and bind VBO.\n");
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 0, NULL, GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    if(glGetError()) printf("error: could not create and bind EBO.\n");
 
     glGenVertexArrays(1, &quad_VAO);
     glBindVertexArray(quad_VAO);
@@ -222,6 +225,9 @@ void frontend_realize_graphics(int scale_factor) {
     glGetProgramiv(effects_program, GL_LINK_STATUS, &status);
     if(status == GL_FALSE || glGetError())
         printf("error: could not attach and link effects shaders.\n");
+
+    texRGB = glGetUniformLocation(effects_program, "texRGB");
+    texDepth = glGetUniformLocation(effects_program, "texDepth");
 }
 
 int view_width, view_height;
@@ -274,8 +280,15 @@ void frontend_render_graphics(void) {
     GLenum drawBuffersList[1] = { GL_COLOR_ATTACHMENT0 };
     glDrawBuffers(1, drawBuffersList);
 
-    glClearColor(0, 0, 0, 1);
+    GLfloat tmp[5];
+    glGetFloatv(GL_COLOR_CLEAR_VALUE, tmp);
+    glGetFloatv(GL_DEPTH_CLEAR_VALUE, tmp + 4);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearDepth(1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(tmp[0], tmp[1], tmp[2], tmp[3]);
+    glClearDepth(tmp[4]);
+
     glUseProgram(shader_program);
     if(status = glGetError()) warning("warning: shader binding did not succeed (0x%x).\n", status);
     
@@ -311,12 +324,14 @@ void frontend_render_graphics(void) {
         //glDrawArrays(GL_LINE_STRIP, 0, element_count); // Interesting results
         if(status = glGetError()) warning("warning: visual rendering did not succeed (0x%x).\n", status);
     }
-    
-    glDisableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    glUseProgram(0);
+
+    glBindVertexArray(quad_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_VBO);
+    if(status = glGetError()) warning("warning: quad VBO binding did not succeed (0x%x).\n", status);
+    glEnableVertexAttribArray(0);
+    if(glGetError()) warning("warning: enabling quad VBO did not succeed.\n");
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    if(status = glGetError()) warning("warning: effects VBO configuration did not succeed (0x%x).\n", status);
     
     /* Now apply effects */
     if (glIsFramebuffer(defaultFBO)) {
@@ -330,29 +345,16 @@ void frontend_render_graphics(void) {
     if(status = glGetError()) warning("warning: framebuffer binding did not succeed (0x%x).\n", status);
     glUseProgram(effects_program);
     if(status = glGetError()) warning("warning: effects shader binding did not succeed (0x%x).\n", status);
-    
-    glBindVertexArray(quad_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quad_VBO);
-    if(status = glGetError()) warning("warning: quad VBO binding did not succeed (0x%x).\n", status);
-    glEnableVertexAttribArray(0);
-    if(glGetError()) warning("warning: enabling quad VBO did not succeed.\n");
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-    if(status = glGetError()) warning("warning: effects VBO configuration did not succeed (0x%x).\n", status);
 
-    GLuint programID;
-    glGetIntegerv(GL_CURRENT_PROGRAM, (GLint*)&programID);
-
-    GLuint attr_tex = glGetUniformLocation(programID, "tex");
     glActiveTexture(GL_TEXTURE0 + 1);
     glBindTexture(GL_TEXTURE_2D, tmp_tex);
     if(status = glGetError()) warning("warning: texture binding did not succeed (0x%x).\n", status);
-    glUniform1i(attr_tex, 1);
+    glUniform1i(texRGB, 1);
 
-    GLuint attr_depth = glGetUniformLocation(programID, "depth");
     glActiveTexture(GL_TEXTURE0 + 2);
     glBindTexture(GL_TEXTURE_2D, depth_tex);
     if(status = glGetError()) warning("warning: depth texture binding did not succeed (0x%x).\n", status);
-    glUniform1i(attr_depth, 2);
+    glUniform1i(texDepth, 2);
 
     glDrawArrays(GL_TRIANGLES, 0, 6);
     if(status = glGetError()) warning("warning: texture rendering did not succeed (0x%x).\n", status);
